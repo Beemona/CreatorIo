949a009b8c22446862c436a526b45e39addb9a6e
Beemona <simona.mihaela.calota@gmail.com>
2024-07-20 20:55:19 +0300

    ----

--- src/components/firstPage.js
+++ src/components/firstPage.js
@@ -10,7 +10,7 @@ const HighlightedText = styled('span')({
   color: '#F663F1',
 });
 
-const CustomTypography = ({ text, highlights }) => {
+const HighlightedTypography = ({ text, highlights }) => {
   const regex = new RegExp(`(${highlights.join('|')})`, 'gi');
   const parts = text.split(regex);
 
@@ -19,12 +19,11 @@ const CustomTypography = ({ text, highlights }) => {
       variant="h6"
       noWrap
       component="a"
-      href="#"
       sx={{
         mb: 2,
         fontFamily: 'Poppins',
         fontWeight: 600,
-        fontSize: '36px',
+        fontSize: '3.6vh',
         color: '#B7472A',
         textDecoration: 'none',
       }}
@@ -40,25 +39,58 @@ const CustomTypography = ({ text, highlights }) => {
   );
 };
 
+const SectionContainer = ({ title, highlights, text }) => {
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        pt: 10,
+        textAlign: 'center',
+      }}
+    >
+      <HighlightedTypography text={title} highlights={highlights} />
+      <Typography
+        variant="h6"
+        noWrap
+        component="a"
+        sx={{
+          mb: 2,
+          fontFamily: 'Poppins',
+          fontWeight: 500,
+          fontSize: '2vh',
+          color: '#B7759E',
+          textDecoration: 'none',
+          ml: 10,
+          mr: 10,
+        }}
+        dangerouslySetInnerHTML={{ __html: text }}
+      />
+    </Container>
+  );
+};
+
 const ScheduleButton = styled(Button)({
   color: '#F663F1',
   fontFamily: 'Poppins',
   borderColor: '#F663F1',
   textTransform: 'none',
-  borderWidth: '3px',
-  borderRadius: '14px', // Add rounded corners
-  width: '344px',
-  height: '67px',
-  fontSize: '20px',
+  borderWidth: '0.3vh',
+  borderRadius: '1.4vh', // Add rounded corners
+  width: '35.4vh',
+  height: '7.2vh',
+  fontSize: '2.4vh',
   fontWeight: '600',
   '&:hover': {
     color: '#B7472A',
     borderColor: '#B7472A',
-    borderWidth: '3px',
-    fontSize: '20px',
-    fontWeight: '600',
+    borderWidth: '0.3vh',
   },
 });
+
+
 function FirstPage() {
   return (
     <div className="App">
@@ -121,12 +153,11 @@ function FirstPage() {
                 variant="h6"
                 noWrap
                 component="a"
-                href="#"
                 sx={{
                   mb: 2,
                   fontFamily: 'Poppins',
                   fontWeight: 500,
-                  fontSize: '34px',
+                  fontSize: '3.4vh',
                   color: '#FFD5AB',
                   textDecoration: 'none',
                 }}
@@ -149,12 +180,11 @@ function FirstPage() {
                 variant="h6"
                 noWrap
                 component="a"
-                href="#"
                 sx={{
                   mb: 2,
                   fontFamily: 'Poppins',
                   fontWeight: 500,
-                  fontSize: '32px',
+                  fontSize: '3.2vh',
                   color: '#FFFFFF',
                   textDecoration: 'none'
                 }}
@@ -165,12 +195,11 @@ function FirstPage() {
                 variant="h6"
                 noWrap
                 component="a"
-                href="#"
                 sx={{
                   mb: 2,
                   fontFamily: 'Poppins',
                   fontWeight: 500,
-                  fontSize: '20px',
+                  fontSize: '2vh',
                   color: '#FFD5AB',
                   textDecoration: 'none'
                 }}
@@ -180,198 +209,245 @@ function FirstPage() {
             </Container>
           </Container>
         </Box>
-      </Box>  <Box sx={{ bgcolor: '#FFD5AB', width: '100%', height: '64px' }} />
-      <Container
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+      </Box>  <Box sx={{ bgcolor: '#FFD5AB', width: '100%', height: '6.4vh' }} />
+ 
+              <SectionContainer
+         title="Descoperă-ți scopul și împlinirea"
+         highlights={['scopul', 'împlinirea']}
+        text="Înțelegerea scopului tău este primul pas către o viață împlinită. Acordă-ți timp pentru a reflecta asupra a ceea ce contează cu<br/>  adevărat pentru tine,identifică-ți pasiunile și aliniază-ți acțiunile cu valorile tale fundamentale."
+      />
+              <SectionContainer
+         title="Încărcă-te cu bunătate față de tine"
+         highlights={['bunătate']}
+        text="Îmbrățișează-ți personalitatea și stilul unic. Celebrează ceea ce te face diferit și exprimă-te autentic. Încrederea vine din a fi fidel <br/>propriei persoane, nu din a te conforma așteptărilor altora."
+      />
 
-        alignItems: 'center', // Align to the left
-        textAlign: 'center', // Align text to the left
-        pt: 14
-      }}>
-    
-    <CustomTypography
-      text="Descoperă-ți scopul și împlinirea"
-      highlights={['scopul', 'împlinirea']}
-    />
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#"
-                sx={{
-                  mb: 2,
-                  fontFamily: 'Poppins',
-                  fontWeight: 500,
-                  fontSize: '20px',
-                  color: '#B7759E',
-                  textDecoration: 'none',
-                  ml: 10,
-                  mr: 10
-                }}
-              >
-Înțelegerea scopului tău este primul pas către o viață împlinită. Acordă-ți timp pentru a reflecta asupra a ceea ce contează cu<br/>  adevărat pentru tine,identifică-ți pasiunile și aliniază-ți acțiunile cu valorile tale fundamentale.              </Typography>
-              </Container>
-              <Container
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-        alignItems: 'center', // Align to the left
-        textAlign: 'center', // Align text to the left
-        pt: 14
-      }}>
-    
-    <CustomTypography
-      text="Încărcă-te cu bunătate față de tine"
-      highlights={['bunătate']}
-    />
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#"
-                sx={{
-                  mb: 2,
-                  fontFamily: 'Poppins',
-                  fontWeight: 500,
-                  fontSize: '20px',
-                  color: '#B7759E',
-                  textDecoration: 'none',
-                  ml: 10,
-                  mr: 10
-                }}
-              >
-                Îmbrățișează-ți personalitatea și stilul unic. Celebrează ceea ce te face diferit și exprimă-te autentic. Încrederea vine din a fi fidel <br/>propriei persoane, nu din a te conforma așteptărilor altora.
-</Typography>
-              </Container>
-             
-              <Container
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-        alignItems: 'center', // Align to the left
-        textAlign: 'center', // Align text to the left
-        pt: 14
-      }}>
-    
-    <CustomTypography
-      text="Reclamă-ți energia"
-      highlights={['energia']}
-    />
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#"
-                sx={{
-                  mb: 2,
-                  fontFamily: 'Poppins',
-                  fontWeight: 500,
-                  fontSize: '20px',
-                  color: '#B7759E',
-                  textDecoration: 'none',
-                  ml: 10,
-                  mr: 10
-                }}
-              >
-Protejează-ți energia stabilind limite sănătoase și prioritizând activitățile care te revigorează. Implică-te în practici care îți refac <br/> spiritul, cum ar fi meditația, exercițiile fizice sau hobby-urile.             
-</Typography> </Container>
-              <Container
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-        alignItems: 'center', // Align to the left
-        textAlign: 'center', // Align text to the left
-        pt: 14
-      }}>
-    
-    <CustomTypography
-      text="Restabilește echilibrul și pacea"
-      highlights={['echilibrul', 'pacea']}
-    />
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#"
-                sx={{
-                  mb: 2,
-                  fontFamily: 'Poppins',
-                  fontWeight: 500,
-                  fontSize: '20px',
-                  color: '#B7759E',
-                  textDecoration: 'none',
-                  ml: 10,
-                  mr: 10
-                }}
-              >
-Tinde spre o viață echilibrată gestionându-ți eficient timpul și asigurându-te că ai un echilibru sănătos între muncă și viața <br/> personală. Fă-ți timp pentru relaxare, relații și auto-îngrijire pentru a menține pacea interioară.     </Typography>         </Container>
-     <Container
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-        alignItems: 'center', // Align to the left
-        textAlign: 'center', // Align text to the left
-        pt: 14
-      }}>
-    
-    <CustomTypography
-      text="Construiește încrederea de neclintit"
-      highlights={['încrederea']}
-    />
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#"
-                sx={{
-                  mb: 2,
-                  fontFamily: 'Poppins',
-                  fontWeight: 500,
-                  fontSize: '20px',
-                  color: '#B7759E',
-                  textDecoration: 'none',
-                  ml: 10,
-                  mr: 10
-                }}
-              >
-Dezvoltă încrederea stabilindu-ți și atingând obiective mici, ieșind din zona ta de confort și învățând din experiențele tale. Ai <br/>încredere în abilitățile tale și crede în potențialul tău de a depăși provocările.</Typography>
-              </Container>
+<SectionContainer
+         title="Reclamă-ți energia"
+         highlights={['energia']}
+        text="Protejează-ți energia stabilind limite sănătoase și prioritizând activitățile care te revigorează. Implică-te în practici care îți refac <br/> spiritul, cum ar fi meditația, exercițiile fizice sau hobby-urile.             "
+      />
+<SectionContainer
+          title="Restabilește echilibrul și pacea"
+          highlights={['echilibrul', 'pacea']}
+        text="Tinde spre o viață echilibrată gestionându-ți eficient timpul și asigurându-te că ai un echilibru sănătos între muncă și viața <br/> personală. Fă-ți timp pentru relaxare, relații și auto-îngrijire pentru a menține pacea interioară.     "
+      />
+              <SectionContainer
+          title="Construiește încrederea de neclintit"
+          highlights={['încrederea']}
+          text="Dezvoltă încrederea stabilindu-ți și atingând obiective mici, ieșind din zona ta de confort și învățând din experiențele tale. Ai <br/>încredere în abilitățile tale și crede în potențialul tău de a depăși provocările.</Typography>"
+          />
               <Container
                sx={{
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
-        
                 alignItems: 'center', // Align to the left
                 textAlign: 'center', // Align text to the left
-                pt: 14
+                pt: 10
               }}>
-                <CustomTypography
-      text="~ Descoperă-te prin Life Coaching ~"
-      highlights={['Life Coaching ~']}
+                <HighlightedTypography
+                text="~ Descoperă-te prin Life Coaching ~"
+                highlights={['Life Coaching ~']}
     />
      <ScheduleButton variant="outlined">Hai să ne cunoaștem!</ScheduleButton>
               </Container>
-              <Box sx={{ bgcolor: '#FFD5AB', width: '100%', height: '750px', mt: 16, mb: 16 }} />
 
-             
+              <Box 
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignItems: 'center', // Align to the left
+                textAlign: 'center', // Align text to the left
+                bgcolor: '#FFD5AB',
+                width: '100%',
+                height: '75vh',
+                mt: 16,
+                mb: 16,  
+                }}>
+                <Typography
+                 variant="h6"
+                 noWrap
+                 component="a"
+                 sx={{
+                   mb: 2,
+                   fontFamily: 'Poppins',
+                   fontWeight: 400,
+                   fontSize: '4.8vh',
+                   letterSpacing: '0.20em',
+                   color: '#DB8530',
+                   textDecoration: 'none',
+                   ml: 10,
+                   mr: 10,
+                   pb: 9
+                 }}>
+                  Mindset Coaching
+                </Typography>
+                <Typography
+                 variant="h6"
+                 noWrap
+                 component="a"
+                 sx={{
+                   mb: 2,
+                   fontFamily: 'Poppins',
+                   fontWeight: 400,
+                   fontSize: '2.4vh',
+                   color: '#B7472A',
+                   textDecoration: 'none',
+                   ml: 10,
+                   mr: 10,
+                   pb: 6
+                 }}>
+Împreună, ne vom confrunta cu fricile și anxietățile tale, transformându-le în oportunități de<br/> învățare și daruri pentru a te schimba în bine. 
+                </Typography>
+                <Typography
+                 variant="h6"
+                 noWrap
+                 component="a"
+                 sx={{
+                   mb: 2,
+                   fontFamily: 'Poppins',
+                   fontWeight: 400,
+                   fontSize: '2.4vh',
+                   color: '#B7472A',
+                   textDecoration: 'none',
+                   ml: 10,
+                   mr: 10,
+                   pb: 6
+                 }}>
+                Vei crea schimbări pozitive, ceea ce înseamnă că poți trăi viața  într-un<br/> mod care să fie împlinitor, semnificativ, plin de bucurie și<br/> implinire.
+                </Typography>
+                <ScheduleButton 
+                sx={{
+                  color: '#B7472A',
+                  borderColor: '#B7472A',
+                  '&:hover': {
+                    color: '#DB8530',
+                    borderColor: '#DB8530'},
+                }} 
+                variant="outlined">Dezvoltă-te cu mine!</ScheduleButton>
+              </Box>
+              <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        alignItems: 'center', // Align to the left
+        textAlign: 'center', // Align text to the left
+
+      }}>
+        <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                sx={{
+                  mb: 2,
+                  fontFamily: 'Poppins',
+                  fontWeight: 600,
+                  fontSize: '3.6vh',
+                  color: '#B7759E',
+                  textDecoration: 'none',
+                  ml: 10,
+                  mr: 10
+                }}
+              >
+Vrei să devii un lider mai bun?   
+</Typography>
+   <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                sx={{
+                  mb: 2,
+                  fontFamily: 'Poppins',
+                  fontWeight: 600,
+                  fontSize: '4.8vh',
+                  color: '#B7472A',
+                  textDecoration: 'none',
+                  ml: 10,
+                  mr: 10,
+                  pb: 5
+                }}
+              >
+Îmbunătățește-ți abilitățile de coach pentru conducere!   </Typography>
+<Box
+            component="img"
+            src="/handshake-leadership.png"
+            alt="First Page"
+            sx={{
+              display: { xs: 'none', md: 'flex', width: '11vh' },
+            }}
+          />
+      </Container>
+              <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        pt: 7,
+        alignItems: 'center', // Align to the left
+        textAlign: 'center', // Align text to the left
+
+      }}>
+              <HighlightedTypography
+      text="Autocunoaștere"
+      highlights={['']}
+    />
+     <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                sx={{
+                  mb: 2,
+                  fontFamily: 'Poppins',
+                  fontWeight: 500,
+                  fontSize: '2vh',
+                  color: '#B7759E',
+                  textDecoration: 'none',
+                  ml: 10,
+                  mr: 10
+                }}
+              >
+   Ajută liderii să-și înțeleagă mai bine punctele forte și slăbiciunile, stilul de conducere și impactul pe care îl au asupra echipei și <br/>organizației.
+   </Typography>
+               </Container>
+
+               <SectionContainer
+          title="Setarea și atingerea obiectivelor"
+          highlights={['']}
+          text="Sprijină liderii în stabilirea unor obiective clare și realiste și în dezvoltarea unor planuri de acțiune pentru a le atinge."
+          />
+               <SectionContainer
+          title="Dezvoltarea competențelor"
+          highlights={['']}
+          text="Se concentrează pe îmbunătățirea abilităților de comunicare, delegare, gestionare a conflictelor și luare a deciziilor."
+          />
+               <SectionContainer
+          title="Feedback și reflecție"
+          highlights={['']}
+          text="  Oferă un spațiu sigur pentru a primi feedback constructiv și pentru a reflecta asupra comportamentului și deciziilor anterioare.   "
+          />
+               <SectionContainer
+          title="Sprijin și responsabilitate"
+          highlights={['']}
+          text="Furnizează suport continuu și responsabilizare, ajutând liderii să rămână concentrați și motivați."
+          />
+          <ScheduleButton 
+                sx={{
+                  mt: 10,
+                  color: '#B7759E',
+                  borderColor: '#B7759E',
+                  '&:hover': {
+                    color: '#F663F1',
+                    borderColor: '#F663F1'},
+                }} 
+                variant="outlined">Progreamează o ședintă</ScheduleButton>
               </div>
               
   );
